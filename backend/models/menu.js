c047924696a529ef04399a5c9cd730560f46ce0c
chethanbhat7 <chethanbhat777@gmail.com>
2026-06-24 03:36:33 +0530

    Added Payment Gateway

--- backend/models/menu.js
+++ backend/models/menu.js
@@ -1,8 +1,27 @@
 const mongoose=require('mongoose')
 
 const menuSchema=new mongoose.Schema({
-
-})
+    menu:[
+        {
+            category:{type:String},
+            items:[
+                {
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:"FoodItem"
+                }
+            ]
+        }
+    ],
+    restaurant:{
+                    type:mongoose.Schema.Types.ObjectId,
+                    ref:"Restaurant"
+    },
+    },
+    {
+        toJSON:{virtuals:true},
+        toObject:{virtuals:true}
+    } 
+)
 
 
 Menu=mongoose.model('Menu',menuSchema)
